8d9622b6afefc2406fe7a32a3e96540de7ddfee7
Abhinav <singh09.abhinav@gmail.com>
2017-07-21 15:26:03 +0530

    added topolgical Ordering

---
+++ src/topologyOrdering/TopologicalOrdering.java
@@ -0,0 +1,29 @@
+package topologyOrdering;
+
+import java.util.Stack;
+
+public class TopologicalOrdering {
+
+	private Stack<Vertex> stack;
+
+	public TopologicalOrdering() {
+		this.stack = new Stack<>();
+	}
+
+	public void dfs(Vertex vertex) {
+
+		vertex.setVisited(true);
+
+		for (Vertex v : vertex.getNeighbourList()) {
+			if (!v.isVisited()) {
+				v.setVisited(true);
+				dfs(v);
+			}
+		}
+		stack.push(vertex);
+	}
+	
+	public Stack<Vertex> getStack(){
+		return this.stack;
+	}
+}
